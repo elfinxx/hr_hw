7d4d10b8354184093853191f080d9ab459451d70
elfinx <elfinx@gmail.com>
2013-05-28 00:10:39 +0900

    VariableGuesser 수정 완료.
    guess 메소드를 TermSet을 받아들이는 것으로 수정.
    기존의 guess 메소드는 TermSet를 받아들이는 메소드로 delegate.
    
    내부적으로 string p+q=r, 즉 string 3개 만을 한정으로 작동하는 메소드들이 많기 때문에 완벽하지 않음.

--- src/main/VariableGuesser.java
+++ src/main/VariableGuesser.java
@@ -16,150 +16,100 @@ public class VariableGuesser {
 	}
 
 	public int guess(TermSet termSet) {
-		
+
 		List<Character> varList;
-		List<List<Integer>> caseSetList;
+		List<List<Integer>> caseList;
 		TermSet aSet;
 		int count = 0;
-		
+
 		varList = buildVariableList(termSet);
-		caseSetList = buildPossiableCaseList(varList.size());
-		
-		for (int i = 0; i < caseSetList.size(); i++) {
-			aSet = replaceVariableToNumber(termSet, varList, caseSetList.get(i));
-			
+		caseList = buildPossiableCaseList(varList.size());
+
+		for (int i = 0; i < caseList.size(); i++) {
+			aSet = replaceVariableToNumber(termSet, varList, caseList.get(i));
 			if (isMatchExpression(aSet.getP(), aSet.getQ(), aSet.getR())) {
 				count++;
 			}
 		}
 		return count;
 	}
-	
+
 	public int guess(String p, String q, String r) {
 
-		
 		TermSet aSet = new TermSet(p, q, r);
 		int count = this.guess(aSet);
-		
+
 		return count;
-		/*
-		List<Character> varList;
-		List<List<Integer>> totalCaseSetList;
-
-		String p_tmp;
-		String q_tmp;
-		String r_tmp;
-
-		int count = 0;
-
-		varList = buildVariableList(p+q+r);
-		totalCaseSetList = buildPossiableCaseList(varList.size());
-
-		for (int i = 0; i < totalCaseSetList.size(); i++) {
-			p_tmp = p.toString();
-			q_tmp = q.toString();
-			r_tmp = r.toString();
-
-			for (int n = 0; n < varList.size(); n++) {
-				p_tmp = p_tmp.replace(varList.get(n), totalCaseSetList.get(i)
-						.get(n).toString().charAt(0));
-				q_tmp = q_tmp.replace(varList.get(n), totalCaseSetList.get(i)
-						.get(n).toString().charAt(0));
-				r_tmp = r_tmp.replace(varList.get(n), totalCaseSetList.get(i)
-						.get(n).toString().charAt(0));
-			}
-
-			if (isMatchExpression(p_tmp, q_tmp, r_tmp)) {
-				count++;
-			}
-		}
-
-		System.out.println(count + "가지");
-		return count;*/
-
 	}
 
 	private TermSet replaceVariableToNumber(TermSet termSet, List<Character> varList, List<Integer> CaseSetList) {
-		
+
 		TermSet aNumberTermSet = termSet.clone();
-		
-		for (int n = 0; n < varList.size(); n++) {			
-			aNumberTermSet.replaceVariableToNumber(varList.get(n), CaseSetList.get(n));
-		}			
+		for (int n = 0; n < varList.size(); n++) {
+			aNumberTermSet = aNumberTermSet.replaceVariableToNumber(varList.get(n), CaseSetList.get(n));
+		}
 		return aNumberTermSet;
 	}
 
-	
+	// 구현 상의 한계점. p+q=r 의 조건이 어떻게 변경될 지 몰라 남겨둠.
 	private boolean isMatchExpression(String p, String q, String r) {
-		
+
 		if (p.charAt(0) == '0' || q.charAt(0) == '0' || r.charAt(0) == '0') {
 			return false;
 		}
+		
 		if (Integer.parseInt(p) + Integer.parseInt(q) == Integer.parseInt(r)) {
 			System.out.println(p + "+" + q + "=" + r);
 			return true;
-		}
-		else
+		} else
 			return false;
 	}
 
-	private List<Character> buildVariableList(String term) {
-
-		List<Character> varList = new ArrayList<Character>();
-		
-		for (int i = 0; i < term.length(); i++) {
-			char curC = term.charAt(i);
-
-			if (isVariable(curC)) {
-				if (!varList.contains(curC)) {
-					varList.add(curC);
-				}
-			}
-		}
-		
-		return varList;
-	}
-	
 	private List<Character> buildVariableList(TermSet termSet) {
 
 		List<Character> varList = new ArrayList<Character>();
 		String term = termSet.getP() + termSet.getQ() + termSet.getR();
-		
+
 		for (int i = 0; i < term.length(); i++) {
 			char curC = term.charAt(i);
-
 			if (isVariable(curC)) {
 				if (!varList.contains(curC)) {
 					varList.add(curC);
 				}
 			}
 		}
-		
 		return varList;
 	}
 
 	private List<List<Integer>> buildPossiableCaseList(int numberOfVariable) {
 
-		List<List<Integer>> totalSet = new ArrayList<List<Integer>>();
-		int numberOfLoop = (int)Math.pow(10, numberOfVariable);
-		String condition = String.format("%d", numberOfVariable);
-		condition = "%" + "0" + condition + "d";
+		List<List<Integer>> CaseList = new ArrayList<List<Integer>>();
+		ArrayList<Integer> caseSet;
+		int numberOfLoop = (int) Math.pow(10, numberOfVariable);
+		String condition = "%" + "0" + numberOfVariable + "d";
 
 		for (int i = 0; i < numberOfLoop; i++) {
 			String str = String.format(condition, i);
-			ArrayList<Integer> set = new ArrayList<Integer>();
-
-			for (int j = 0; j < numberOfVariable; j++) {
-				if (set.contains((int) str.charAt(j) - 48))
-					break;
-				else
-					set.add((int) str.charAt(j) - 48);				
-			}
-
-			if (set.size() == numberOfVariable)
-				totalSet.add(set);
+			
+			caseSet = generateCase(numberOfVariable, str);
+			if (caseSet.size() == numberOfVariable)
+				CaseList.add(caseSet);
+			else
+				caseSet = null;
 		}
-		return totalSet;
+		return CaseList;
+	}
+
+	private ArrayList<Integer> generateCase(int numberOfVariable, String str) {
+		ArrayList<Integer> set = new ArrayList<Integer>();
+		
+		for (int i = 0; i < numberOfVariable; i++) {
+			if (set.contains((int) str.charAt(i) - 48))
+				break;
+			else
+				set.add((int) str.charAt(i) - 48);
+		}
+		return set;
 	}
 
 	private boolean isVariable(char c) {
@@ -178,35 +128,31 @@ public class VariableGuesser {
 		String r = "100";
 
 		System.out.println(vg.guess(p, q, r));
-		// System.out.println(vg.getNumberOfCase(3));
-
 	}
-	
-	
+
 	class TermSet implements Iterable<String> {
 
 		List<String> termList;
-		Iterator iter;
-		
-		// termList의 deep copy. 
+		Iterator<String> iter;
+
+		// termList의 deep copy.
 		public TermSet(List<String> termList) {
 			this.termList = new ArrayList<String>();
 			this.iter = termList.iterator();
-			
+
 			for (String aTerm : termList) {
-				this.termList.add(aTerm);				
+				this.termList.add(aTerm);
 			}
 		}
-		
+
 		public TermSet(String p, String q, String r) {
-		
 			termList = new ArrayList<String>();
-			
+
 			termList.add(p);
 			termList.add(q);
 			termList.add(r);
 		}
-		
+
 		public String getP() {
 			return termList.get(0);
 		}
@@ -218,20 +164,19 @@ public class VariableGuesser {
 		public String getR() {
 			return termList.get(2);
 		}
-		
-		public TermSet replaceVariableToNumber(char variable, int value) {
 
+		public TermSet replaceVariableToNumber(char variable, int value) {
 			List<String> newList = new ArrayList<String>();
-			
+
 			for (String aTerm : termList) {
-				newList.add(aTerm.replace(variable, (char)(value+48)));
+				newList.add(aTerm.replace(variable, (char) (value + 48)));
 			}
-			return new TermSet(newList);			
+			return new TermSet(newList);
 		}
-		
+
 		// deep copy. clone의 의미에 맞는지.
 		public TermSet clone() {
-			return new TermSet(this.termList); 
+			return new TermSet(this.termList);
 		}
 
 		@Override
